9338643ce0cdd228ad2fe47f49ccd9866e6e99d3
Hari96 <bob@rgbassociates.co.uk>
2015-12-14 23:38:03 +0000

    Updated palindrome-check file

--- palindromes-check.js
+++ palindromes-check.js
@@ -1,7 +1,6 @@
 function palindrome(str) {
   var re = /[\(\)_\-,\.\s]/g;
   str = str.replace(re, "");
-  console.log(str);
   var lcase = str.toLowerCase();
   var strArr = lcase.split("");
   var newStrArr = strArr.reverse();
